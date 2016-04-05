7641d3151f45344ed95f12d49c44f4581dec2388
Shawn Cho <grandpacho@gmail.com>
2016-04-04 22:23:36 -0700

    updating with tmux

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -1,7 +1,23 @@
 #!/bin/sh
-rm ~/.i3/config ~/.i3/i3blocks.conf ~/.vimrc
-# i3
-ln -s ~/.dotfiles/.i3/config ~/.i3/config
-ln -s ~/.dotfiles/.i3/i3blocks.conf ~/.i3/i3blocks.conf
+rm ~/.vimrc ~/.tmux.conf ~/.tmux
+
+uname -a | grep 'Darwin' &> /dev/null
+
+# If this is a Mac OS X
+if [ $? == 0 ]; then
+	# tmux
+	ln -s ~/.dotfiles/.tmux-mac.conf ~/.tmux.conf
+	ln -s ~/.dotfiles/.tmux-mac ~/.tmux
+else
+	rm ~/.i3/config ~/.i3/i3blocks.conf
+	# i3
+	ln -s ~/.dotfiles/.i3/config ~/.i3/config
+	ln -s ~/.dotfiles/.i3/i3blocks.conf ~/.i3/i3blocks.conf
+	# tmux
+	ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
+	ln -s ~/.dotfiles/.tmux ~/.tmux
+fi
+
 # vim
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
+
